4b8036044c8979401335399c6f60f63805416f09
Emil Rubinic <erubinic@riteh.hr>
2022-11-23 20:51:15 +0100

    Add simple streams example pt2.

--- src/hr/unipu/rpii/Main.java
+++ src/hr/unipu/rpii/Main.java
@@ -2,21 +2,31 @@ package hr.unipu.rpii;
 
 import hr.unipu.rpii.calculator.ItdResultsService;
 import hr.unipu.rpii.calculator.impl.ResultsServiceImpl;
+import hr.unipu.rpii.calculator.model.ItdResultResponse;
 import hr.unipu.rpii.calculator.model.Student;
 import hr.unipu.rpii.calculator.studentdata.StudentData;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
     static ItdResultsService itdResultsService = new ResultsServiceImpl();
 
     public static void main(String[] args) {
-       /* var results = StudentData.getStudents()
+        Function<Student, String> getJmbag = Student::getJmbag;
+        Function<String, ItdResultResponse> getItdResult = itdResultsService::getItdResult;
+        Comparator<ItdResultResponse> comparing = Comparator.comparing(it -> Integer.valueOf(it.getJmbag()));
+        Collector<ItdResultResponse, ?, List<ItdResultResponse>> itdResultResponseListCollector = Collectors.toList();
+
+
+        var results = StudentData.getStudents()
                 .stream()
-                .map(Student::getJmbag)
-                .map(itdResultsService::getItdResult)
-                .sorted(Comparator.comparing(it -> Integer.valueOf(it.getJmbag())))
-                .collect(Collectors.toList());*/
+                .map(getJmbag)
+                .map(getItdResult)
+                .sorted(comparing)
+                .collect(itdResultResponseListCollector);
     }
 }
