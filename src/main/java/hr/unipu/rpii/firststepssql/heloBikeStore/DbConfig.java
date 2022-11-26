301aa3a2b1e3134389b2c775241b7897bcaf2f6d
Emil Rubinic <erubinic@riteh.hr>
2022-11-26 09:03:46 +0100

    Add some examples of using database.

---
+++ src/main/java/hr/unipu/rpii/firststepssql/heloBikeStore/DbConfig.java
@@ -0,0 +1,8 @@
+package hr.unipu.rpii.firststepssql.heloBikeStore;
+
+public class DbConfig {
+    public static String url = "jdbc:sqlserver://localhost:1433;" +
+            "database=BikeStores;" +
+            "user=rpuser;" +
+            "password=1234";
+}
---
+++ src/main/java/hr/unipu/rpii/firststepssql/heloBikeStore/HelloBikeStore2.java
@@ -0,0 +1,40 @@
+package hr.unipu.rpii.firststepssql.heloBikeStore;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+public class HelloBikeStore2 {
+    public static void main(String[] args) {
+        SQLServerDataSource sqlDs = new SQLServerDataSource();
+        sqlDs.setIntegratedSecurity(false);
+        sqlDs.setUser("rpuser");
+        sqlDs.setPassword("1234");
+        sqlDs.setDatabaseName("BikeStores");
+        sqlDs.setPortNumber(1433);
+
+        try (Connection conn = sqlDs.getConnection()) {
+            Statement statement = conn.createStatement();
+            String sql = "SELECT * FROM production.brands";
+
+            ResultSet result = statement.executeQuery(sql);
+
+            int rowCounter = 0;
+            while (result.next()) {
+                int id = result.getInt(1); // get by index
+                String brandName = result.getString("brand_name"); // get by column name
+
+                String output = "Result form row %d -> BrandId: %d, BrandName:%s";
+                System.out.println(String.format(output, ++rowCounter, id, brandName));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
